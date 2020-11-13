61ee834f7e58c42c4c695ae0a956c57b9d310392
Bartosz Prochowski <bravoprochu@gmail.com>
2020-11-13 19:12:47 +0100

    update angular 11 devkit

--- src/app/shared/guards/resolve-animation-is-done.guard.spec.ts
+++ src/app/shared/guards/resolve-animation-is-done.guard.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject, waitForAsync } from '@angular/core/testing';
 
 import { ResolveAnimationIsDoneGuard } from './resolve-animation-is-done.guard';
 
