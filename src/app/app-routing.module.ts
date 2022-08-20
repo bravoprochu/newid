2dc3d70e68fc3e2548d80089b6f55adb6eb4e2a1
bravoprochu <bravoprochu@gmail.com>
2022-08-20 20:34:37 +0200

    remove pesonal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,28 +4,36 @@ import { PageNotFoundComponent } from '@sharedComponents/page-not-found/page-not
 import { HomeComponent } from '@sites/home/home.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent, data: {animation: 'Home', title: 'NewId - Nowa Tożsamość Twojej Firmy. Agencja Marketingowa, Agencja Reklamowa. Kładziemy nacisk na wyjątkowość', description: 'Zróbmy wynik 💰 Świeżość 🌱, nieszablonowe 💥indywidualne podejście ☝️ wysoka skuteczność 🎯 olbrzymie zaangażowanie 😅🙋 Spraw Swojej Firmie Prezent ! Reklama Twojego produktu, fotografia, grafika, animacje, code.'} },
-     
-  
-  {path: 'kontakt', loadChildren: ()=> import('@sites/contact/contact.module').then(m=>m.ContactModule), data: {animation: 'Kontakt'}  },
-    {path: 'contact', redirectTo: 'kontakt', pathMatch: 'full'},
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      animation: 'Home',
+      title:
+        'NewId - Nowa Tożsamość Twojej Firmy. Agencja Marketingowa, Agencja Reklamowa. Kładziemy nacisk na wyjątkowość',
+      description:
+        'Zróbmy wynik 💰 Świeżość 🌱, nieszablonowe 💥indywidualne podejście ☝️ wysoka skuteczność 🎯 olbrzymie zaangażowanie 😅🙋 Spraw Swojej Firmie Prezent ! Reklama Twojego produktu, fotografia, grafika, animacje, code.',
+    },
+  },
 
-  {path: 'promocje', loadChildren: ()=> import('@sites/promocje/promocje.module').then(m=>m.PromocjeModule), data: {animation: 'Promocje'} },
-  {path: 'promo', redirectTo: 'promocje', pathMatch: 'full'},
-
-  {path: 'projekty', loadChildren: ()=> import('@sites/projekty/projekty.module').then(m=>m.ProjektyModule), data: {animation: 'Projekty'} },
-
-  
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: '**', component: PageNotFoundComponent},
+  {
+    path: 'kontakt',
+    loadChildren: () =>
+      import('@sites/contact/contact.module').then((m) => m.ContactModule),
+    data: { animation: 'Kontakt' },
+  },
+  { path: 'contact', redirectTo: 'kontakt', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    scrollPositionRestoration: "top",
-    relativeLinkResolution: 'legacy'
-})],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'top',
+      relativeLinkResolution: 'legacy',
+    }),
+  ],
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
